aa12845e4610c9bd0abfbefe7ac7dbb760bd249c
AbdoHerO <abdo.sbeta.12@gmail.com>
2024-06-11 11:45:42 +0100

    Changes pc sophatel - seconde version of filter - OpenCV - SPR - SOPHACA

--- constants.py
+++ constants.py
@@ -2,85 +2,268 @@
 model_settings_magicpro_filter = {
     'GLOBAL': {
         'gaussian_blur': (5, 5),
-        'clahe_clip_limit': 4.5,
-        'clahe_tile_grid_size': (5, 5),
-        'contrast_alpha': 1.5,
-        'brightness_beta': 10,
-        'adaptive_thresh_block_size': 15,
-        'adaptive_thresh_C': 50,
-        'dilate_kernel_size': (1, 1),
-        'dilate_iterations': 1
-    },
-    'SOPHACA': {
-        'gaussian_blur': (5, 5),
-        'clahe_clip_limit': 4.5,
-        'clahe_tile_grid_size': (5, 5),
-        'contrast_alpha': 1.5,
-        'brightness_beta': 10,
-        'adaptive_thresh_block_size': 15,
-        'adaptive_thresh_C': 50,
-        'dilate_kernel_size': (1, 1),
-        'dilate_iterations': 1
-    },
-    'SPR': {
-        'gaussian_blur': (5, 5),
-        'clahe_clip_limit': 2.5,
+        'clahe_clip_limit': 2.8,
         'clahe_tile_grid_size': (100, 100),
         'contrast_alpha': 1.1,
-        'brightness_beta': 20,
-        'adaptive_thresh_block_size': 15,
+        'brightness_beta': 10,
+        'adaptive_thresh_block_size': 85,
         'adaptive_thresh_C': 100,
         'dilate_kernel_size': (1, 1),
         'dilate_iterations': 1,
-        'saturation_factor': 2.0,  # Increase this value to concentrate colors more
-        'h': 10,  # Filter strength for luminance component
-        'hForColorComponents': 10,  # Same as h but for color components
-        'templateWindowSize': 0,  # Size in pixels of the template patch that is used to compute weights
-        'searchWindowSize': 41  # Size in pixels of the window that is used to compute weighted average for given pixel
 
+        'saturation_factor': 4.0,  # Increase this value to concentrate colors more
+        'h': 15,  # Filter strength for luminance component
+        'hForColorComponents': 0,  # Same as h but for color components
+        'templateWindowSize': 0,  # Size in pixels of the template patch that is used to compute weights
+        'searchWindowSize': 45,  # Size in pixels of the window that is used to compute weighted average for given pixel
+
+        'adaptive_thresh_block_size_2': 95,
+        'adaptive_thresh_C_2': 100,
+        'dilate_kernel_size_2': (1, 1),
+        'dilate_iterations_2': 1,
+
+        'black_point': 10,  # Adjust black point
+        # 'warmth': 2,  # Adjust warmth
+        # 'tint': 2,  # Adjust tint
+        # 'brilliance': 80,  # Adjust brilliance
+        # 'exposure': 1.0  # Adjust exposure
+
+        'color_palettes': {
+            'custom': {
+                'hue_shift': 5,
+                'saturation_factor': 0.7,
+                'brightness_beta': 0,
+                'white_intensity': 1.0,  # New parameter to control white intensity
+                'black_intensity': 1.0   # New parameter to control black intensity
+            }
+        }
+    },
+    'SOPHACA': {
+        'gaussian_blur': (5, 5),
+        'clahe_clip_limit': 3.0,
+        'clahe_tile_grid_size': (8, 8),
+        'contrast_alpha': 1.2,
+        'brightness_beta': 10,
+        'adaptive_thresh_block_size': 85,
+        'adaptive_thresh_C': 150,
+        'dilate_kernel_size': (1, 1),
+        'dilate_iterations': 1,
+
+        'saturation_factor': 3.0,  # Increase this value to concentrate colors more
+        'h': 15,  # Filter strength for luminance component
+        'hForColorComponents': 0,  # Same as h but for color components
+        'templateWindowSize': 0,  # Size in pixels of the template patch that is used to compute weights
+        'searchWindowSize': 45,  # Size in pixels of the window that is used to compute weighted average for given pixel
+
+        'adaptive_thresh_block_size_2': 95,
+        'adaptive_thresh_C_2': 100,
+        'dilate_kernel_size_2': (1, 1),
+        'dilate_iterations_2': 1,
+
+        'black_point': 10,  # Adjust black point
+        # 'warmth': 2,  # Adjust warmth
+        # 'tint': 2,  # Adjust tint
+        # 'brilliance': 80,  # Adjust brilliance
+        # 'exposure': 1.0  # Adjust exposure
+
+        'color_palettes': {
+            'custom': {
+                'hue_shift': 0,
+                'saturation_factor': 0.8,
+                'brightness_beta': 10,
+                'white_intensity': 1.0,  # New parameter to control white intensity
+                'black_intensity': 0.4   # New parameter to control black intensity
+            }
+        }
+    },
+    'SPR': {
+        'gaussian_blur': (5, 5),
+        'clahe_clip_limit': 2.8,
+        'clahe_tile_grid_size': (100, 100),
+        'contrast_alpha': 1.1,
+        'brightness_beta': 10,
+        'adaptive_thresh_block_size': 85,
+        'adaptive_thresh_C': 100,
+        'dilate_kernel_size': (1, 1),
+        'dilate_iterations': 1,
+
+        'saturation_factor': 4.0,  # Increase this value to concentrate colors more
+        'h': 15,  # Filter strength for luminance component
+        'hForColorComponents': 0,  # Same as h but for color components
+        'templateWindowSize': 0,  # Size in pixels of the template patch that is used to compute weights
+        'searchWindowSize': 45,  # Size in pixels of the window that is used to compute weighted average for given pixel
+
+        'adaptive_thresh_block_size_2': 95,
+        'adaptive_thresh_C_2': 100,
+        'dilate_kernel_size_2': (1, 1),
+        'dilate_iterations_2': 1,
+
+        'black_point': 10,  # Adjust black point
+        # 'warmth': 2,  # Adjust warmth
+        # 'tint': 2,  # Adjust tint
+        # 'brilliance': 80,  # Adjust brilliance
+        # 'exposure': 1.0  # Adjust exposure
+
+        'color_palettes': {
+            'custom': {
+                'hue_shift': 3,
+                'saturation_factor': 0.7,
+                'brightness_beta': 0,
+                'white_intensity': 1.0,  # New parameter to control white intensity
+                'black_intensity': 1.0   # New parameter to control black intensity
+            }
+        }
     },
     'SOPHADIMS': {
         'gaussian_blur': (5, 5),
-        'clahe_clip_limit': 4.5,
-        'clahe_tile_grid_size': (50, 50),
-        'contrast_alpha': 1.4,
-        'brightness_beta': 50,
-        'adaptive_thresh_block_size': 11,
-        'adaptive_thresh_C': 30,
+        'clahe_clip_limit': 2.8,
+        'clahe_tile_grid_size': (100, 100),
+        'contrast_alpha': 1.1,
+        'brightness_beta': 10,
+        'adaptive_thresh_block_size': 85,
+        'adaptive_thresh_C': 100,
         'dilate_kernel_size': (1, 1),
-        'dilate_iterations': 3
+        'dilate_iterations': 1,
+
+        'saturation_factor': 4.0,  # Increase this value to concentrate colors more
+        'h': 15,  # Filter strength for luminance component
+        'hForColorComponents': 0,  # Same as h but for color components
+        'templateWindowSize': 0,  # Size in pixels of the template patch that is used to compute weights
+        'searchWindowSize': 45,  # Size in pixels of the window that is used to compute weighted average for given pixel
+
+        'adaptive_thresh_block_size_2': 95,
+        'adaptive_thresh_C_2': 100,
+        'dilate_kernel_size_2': (1, 1),
+        'dilate_iterations_2': 1,
+
+        'black_point': 10,  # Adjust black point
+        # 'warmth': 2,  # Adjust warmth
+        # 'tint': 2,  # Adjust tint
+        # 'brilliance': 80,  # Adjust brilliance
+        # 'exposure': 1.0  # Adjust exposure
+
+        'color_palettes': {
+            'custom': {
+                'hue_shift': 5,
+                'saturation_factor': 0.7,
+                'brightness_beta': 0,
+                'white_intensity': 1.0,  # New parameter to control white intensity
+                'black_intensity': 1.0   # New parameter to control black intensity
+            }
+        }
     },
     'GPM': {
         'gaussian_blur': (5, 5),
-        'clahe_clip_limit': 1.5,
-        'clahe_tile_grid_size': (30, 30),
-        'contrast_alpha': 1.3,
-        'brightness_beta': 30,
-        'adaptive_thresh_block_size': 15,
-        'adaptive_thresh_C': 30,
+        'clahe_clip_limit': 2.8,
+        'clahe_tile_grid_size': (100, 100),
+        'contrast_alpha': 1.1,
+        'brightness_beta': 10,
+        'adaptive_thresh_block_size': 85,
+        'adaptive_thresh_C': 100,
         'dilate_kernel_size': (1, 1),
-        'dilate_iterations': 1
+        'dilate_iterations': 1,
+
+        'saturation_factor': 4.0,  # Increase this value to concentrate colors more
+        'h': 15,  # Filter strength for luminance component
+        'hForColorComponents': 0,  # Same as h but for color components
+        'templateWindowSize': 0,  # Size in pixels of the template patch that is used to compute weights
+        'searchWindowSize': 45,  # Size in pixels of the window that is used to compute weighted average for given pixel
+
+        'adaptive_thresh_block_size_2': 95,
+        'adaptive_thresh_C_2': 100,
+        'dilate_kernel_size_2': (1, 1),
+        'dilate_iterations_2': 1,
+
+        'black_point': 10,  # Adjust black point
+        # 'warmth': 2,  # Adjust warmth
+        # 'tint': 2,  # Adjust tint
+        # 'brilliance': 80,  # Adjust brilliance
+        # 'exposure': 1.0  # Adjust exposure
+
+        'color_palettes': {
+            'custom': {
+                'hue_shift': 5,
+                'saturation_factor': 0.7,
+                'brightness_beta': 0,
+                'white_intensity': 1.0,  # New parameter to control white intensity
+                'black_intensity': 1.0   # New parameter to control black intensity
+            }
+        }
     },
     'RECAMED': {
-        'gaussian_blur': (3, 3),
-        'clahe_clip_limit': 6.0,
-        'clahe_tile_grid_size': (30, 30),
-        'contrast_alpha': 1.7,
-        'brightness_beta': 40,
-        'adaptive_thresh_block_size': 11,
-        'adaptive_thresh_C': 25,
+        'gaussian_blur': (5, 5),
+        'clahe_clip_limit': 2.8,
+        'clahe_tile_grid_size': (100, 100),
+        'contrast_alpha': 1.1,
+        'brightness_beta': 10,
+        'adaptive_thresh_block_size': 85,
+        'adaptive_thresh_C': 100,
         'dilate_kernel_size': (1, 1),
-        'dilate_iterations': 2
+        'dilate_iterations': 1,
+
+        'saturation_factor': 4.0,  # Increase this value to concentrate colors more
+        'h': 15,  # Filter strength for luminance component
+        'hForColorComponents': 0,  # Same as h but for color components
+        'templateWindowSize': 0,  # Size in pixels of the template patch that is used to compute weights
+        'searchWindowSize': 45,  # Size in pixels of the window that is used to compute weighted average for given pixel
+
+        'adaptive_thresh_block_size_2': 95,
+        'adaptive_thresh_C_2': 100,
+        'dilate_kernel_size_2': (1, 1),
+        'dilate_iterations_2': 1,
+
+        'black_point': 10,  # Adjust black point
+        # 'warmth': 2,  # Adjust warmth
+        # 'tint': 2,  # Adjust tint
+        # 'brilliance': 80,  # Adjust brilliance
+        # 'exposure': 1.0  # Adjust exposure
+
+        'color_palettes': {
+            'custom': {
+                'hue_shift': 5,
+                'saturation_factor': 0.7,
+                'brightness_beta': 0,
+                'white_intensity': 1.0,  # New parameter to control white intensity
+                'black_intensity': 1.0   # New parameter to control black intensity
+            }
+        }
     },
     'COOPER': {
-        'gaussian_blur': (3, 3),
-        'clahe_clip_limit': 3.5,
-        'clahe_tile_grid_size': (35, 35),
-        'contrast_alpha': 1.4,
-        'brightness_beta': 35,
-        'adaptive_thresh_block_size': 9,
-        'adaptive_thresh_C': 30,
+        'gaussian_blur': (5, 5),
+        'clahe_clip_limit': 2.8,
+        'clahe_tile_grid_size': (100, 100),
+        'contrast_alpha': 1.1,
+        'brightness_beta': 10,
+        'adaptive_thresh_block_size': 85,
+        'adaptive_thresh_C': 100,
         'dilate_kernel_size': (1, 1),
-        'dilate_iterations': 3
+        'dilate_iterations': 1,
+
+        'saturation_factor': 4.0,  # Increase this value to concentrate colors more
+        'h': 15,  # Filter strength for luminance component
+        'hForColorComponents': 0,  # Same as h but for color components
+        'templateWindowSize': 0,  # Size in pixels of the template patch that is used to compute weights
+        'searchWindowSize': 45,  # Size in pixels of the window that is used to compute weighted average for given pixel
+
+        'adaptive_thresh_block_size_2': 95,
+        'adaptive_thresh_C_2': 100,
+        'dilate_kernel_size_2': (1, 1),
+        'dilate_iterations_2': 1,
+
+        'black_point': 10,  # Adjust black point
+        # 'warmth': 2,  # Adjust warmth
+        # 'tint': 2,  # Adjust tint
+        # 'brilliance': 80,  # Adjust brilliance
+        # 'exposure': 1.0  # Adjust exposure
+
+        'color_palettes': {
+            'custom': {
+                'hue_shift': 5,
+                'saturation_factor': 0.7,
+                'brightness_beta': 0,
+                'white_intensity': 1.0,  # New parameter to control white intensity
+                'black_intensity': 1.0   # New parameter to control black intensity
+            }
+        }
     }
 }
--- magic_pro_filter.py
+++ magic_pro_filter.py
@@ -18,7 +18,40 @@ def show_image(image, title="Image"):
     plt.axis('off')
     plt.show()
 
-def apply_magic_pro_filter(image_path, output_path, model):
+def apply_color_palette(image, palette_settings):
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    h, s, v = cv2.split(hsv)
+
+    # Apply hue shift
+    h = (h.astype(int) + palette_settings.get('hue_shift', 0)) % 180
+    h = h.astype(np.uint8)
+
+    # Apply saturation factor
+    s = cv2.multiply(s, palette_settings.get('saturation_factor', 1.0))
+
+    # Merge the channels back and convert to BGR
+    hsv_colored = cv2.merge([h, s, v])
+    image = cv2.cvtColor(hsv_colored, cv2.COLOR_HSV2BGR)
+
+    # Apply brightness adjustment
+    image = cv2.convertScaleAbs(image, alpha=1.0, beta=palette_settings.get('brightness_beta', 0))
+
+    # Apply white intensity adjustment
+    white_intensity = palette_settings.get('white_intensity', 1.0)
+    white_mask = cv2.inRange(image, np.array([200, 200, 200]), np.array([255, 255, 255]))
+    if white_intensity != 1.0:
+        image[white_mask != 0] = cv2.convertScaleAbs(image[white_mask != 0], alpha=white_intensity, beta=0)
+
+    # Apply black intensity adjustment
+    black_intensity = palette_settings.get('black_intensity', 1.0)
+    black_mask = cv2.inRange(image, np.array([0, 0, 0]), np.array([50, 50, 50]))
+    if black_intensity != 1.0:
+        image[black_mask != 0] = cv2.convertScaleAbs(image[black_mask != 0], alpha=black_intensity, beta=0)
+
+    return image
+
+
+def apply_magic_pro_filter(image_path, output_path, model, palette_name=None):
     settings = constants.model_settings_magicpro_filter[model]
 
     image = cv2.imread(image_path)
@@ -78,11 +111,48 @@ def apply_magic_pro_filter(image_path, output_path, model):
     hsv_colored = cv2.merge([h, s, v])
     final_image = cv2.cvtColor(hsv_colored, cv2.COLOR_HSV2BGR)
 
+    """-----------thicken the font-----------"""
+
+    # Apply additional filter to thicken the font further
+    gray_final = cv2.cvtColor(final_image, cv2.COLOR_BGR2GRAY)
+    binary_final = cv2.adaptiveThreshold(gray_final, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,
+                                         settings['adaptive_thresh_block_size_2'], settings['adaptive_thresh_C_2'])
+    kernel_final = np.ones(settings['dilate_kernel_size_2'], np.uint8)
+    thickened_final = cv2.dilate(binary_final, kernel_final, iterations=settings['dilate_iterations_2'])
+
+    final_image_colored_thickened = cv2.bitwise_and(final_image, final_image, mask=thickened_final)
+
+
+    """------------black_point----------"""
+    # Convert to LAB color space
+    lab = cv2.cvtColor(final_image_colored_thickened, cv2.COLOR_BGR2LAB)
+    l, a, b = cv2.split(lab)
+
+    # Apply Black Point adjustment
+    black_point = settings['black_point']
+    l = cv2.addWeighted(l, 1.0, l, 0, -black_point)
+
+    lab = cv2.merge((l, a, b))
+    final_image_colored_thickened = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
+
+    """ -----------color picker----------- """
+    # Apply color palette if specified
+    if palette_name and palette_name in settings['color_palettes']:
+        palette_settings = settings['color_palettes'][palette_name]
+        final_image_colored_thickened = apply_color_palette(final_image_colored_thickened, palette_settings)
+
+
     # Save the result
-    cv2.imwrite(output_path, final_image)
+    cv2.imwrite(output_path, final_image_colored_thickened)
     print(f"Filtered image saved to {output_path}")
 
 if __name__ == "__main__":
-    input_image_path = 'output/cropped_spr_iphn.jpg'
-    output_image_path = 'output/filtered_cropped_spr_iphn.jpg'
-    apply_magic_pro_filter(input_image_path, output_image_path, 'SPR')
+    palette_name = 'custom'  # Change to the desired palette name
+
+    input_image_path = 'output/cropped_sophaca_iphn.jpg'
+    output_image_path = 'output/filtered_cropped_sophaca_iphn.jpg'
+    apply_magic_pro_filter(input_image_path, output_image_path, 'SOPHACA', palette_name)
+
+    # input_image_path = 'output/cropped_spr_iphn.jpg'
+    # output_image_path = 'output/filtered_cropped_spr_iphn.jpg'
+    # apply_magic_pro_filter(input_image_path, output_image_path, 'SPR', palette_name)
